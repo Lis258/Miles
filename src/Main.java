d22864b37352f85d92d5f60bff8a781dd55f20bb
Lis258 <avb-90@ya.ru>
2021-01-09 22:52:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 5963;
+        int divisor = 20;
+        int bonus = cost / divisor;
+        System.out.println(bonus);
+    }
+}
